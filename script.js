eaba609f9f1c13596d1da71c7c72467029d357bd
joaopedrobrgs <joaopedrobrgsantos@gmail.com>
2023-10-18 19:22:41 -0300

    Alterei estrutura da página para o que o trabalho solicitava / Adicionei comentários

--- script.js
+++ script.js
@@ -1,40 +1,10 @@
-const sobre = document.getElementById("sobre");
-const formacao = document.getElementById("formacao");
-const portfolio = document.getElementById("portfolio");
-const contato = document.getElementById("contato");
+//Pegando elementos no Documento Object Model:
 const formContact = document.getElementById("form-contact");
 const inputs = document.getElementsByClassName("input-text");
 
-function showSobre(e){
-  sobre.classList.remove('hide');
-  formacao.classList.add('hide');
-  portfolio.classList.add('hide');
-  contato.classList.add('hide');
-}
-
-function showFormacao(e){
-  formacao.classList.remove('hide');
-  sobre.classList.add('hide');
-  portfolio.classList.add('hide');
-  contato.classList.add('hide');
-}
-
-function showPortfolio(e){
-  portfolio.classList.remove('hide');
-  sobre.classList.add('hide');
-  formacao.classList.add('hide');
-  contato.classList.add('hide');
-}
-
-function showContato(e){
-  contato.classList.remove('hide');
-  sobre.classList.add('hide');
-  portfolio.classList.add('hide');
-  formacao.classList.add('hide');
-}
-
+//Adicionando um listener no evento de enviar formulário e criando função para 
+//definir o que vai acontecer a partir disso:
 formContact.addEventListener("submit", enviarMensagem)
-
 function enviarMensagem(e){
   e.preventDefault();
   alert("Mensagem enviada com sucesso!")
