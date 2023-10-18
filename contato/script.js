d164267598b8a63b7cd6d9054e02c32483383adc
joaopedrobrgs <joaopedrobrgsantos@gmail.com>
2023-10-18 19:47:58 -0300

    Alterei estrutura de pastas do projeto

--- contato/script.js
+++ contato/script.js
@@ -2,9 +2,9 @@
 const formContact = document.getElementById("form-contact");
 const inputs = document.getElementsByClassName("input-text");
 
-//Adicionando um listener no evento de enviar formulário e criando função para 
-//definir o que vai acontecer a partir disso:
+//Adicionando um listener no evento de enviar formulário:
 formContact.addEventListener("submit", enviarMensagem)
+//Criando função para definir o que vai acontecer quando evento for executado:
 function enviarMensagem(e){
   e.preventDefault();
   alert("Mensagem enviada com sucesso!")
